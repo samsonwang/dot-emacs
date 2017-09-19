b6e4ec01488375ad613cc221792f0ce3a043e157
samson <zl.wang@qq.com>
2017-09-19 11:34:51 +0800

    use helm occur isearch instead of swiper

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -2,18 +2,12 @@
 
 
 (require 'helm-config)
-
-
 (helm-mode 1)
 
-
 ;; keybindings
-(global-set-key (kbd "C-s") 'swiper)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "M-x") 'helm-M-x)
-
-
+(global-set-key (kbd "C-s")        'helm-occur-from-isearch)
+(global-set-key (kbd "C-x C-f")    'helm-find-files)
+(global-set-key (kbd "M-x")        'helm-M-x)
 
 (provide 'init-helm)
-;;;
-
+;;; init-helm ends here
