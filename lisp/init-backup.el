24881b24232ba7d7f222c6acdf3e5af896efc8a2
SamsonWang <zl.wang@foxmail.com>
2017-09-22 10:43:51 +0800

    fix autosave dir

--- lisp/init-backup.el
+++ lisp/init-backup.el
@@ -3,8 +3,8 @@
 
 ; create the autosave dir if necessary, since emacs won't.
 
-(setq autosave-dir (concat config-home ".emacs_autosaves") )
-(setq backup-dir (concat config-home ".emacs_backups") )
+(setq autosave-dir (concat config-home ".emacs_autosaves/") )
+(setq backup-dir (concat config-home ".emacs_backups/") )
 
 (if (not (file-exists-p autosave-dir))
   (make-directory autosave-dir) )
@@ -14,14 +14,17 @@
 (setq
    backup-by-copying t ; don't clobber symlinks
    backup-directory-alist
-    '( ( "." . "~/.emacs.d/.emacs_backups" ) ) ; don't litter my fs tree
+      '( ( "." . "~/.emacs.d/.emacs_backups/" ) ) ; don't litter my fs tree
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
    version-control t) ; use versioned backups
 
 (setq auto-save-file-name-transforms
-   '( (".*" "~/.emacs.d/.emacs_autosaves" t) ) )
+   '( (".*" "~/.emacs.d/.emacs_autosaves/" t) ) )
+
+(setq auto-save-list-file-prefix
+      "~/.emacs.d/.emacs_autosaves/")
 
 ;;; init-backup-func.el ends here
 (provide 'init-backup)
