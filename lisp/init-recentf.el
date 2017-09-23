c1756506937cd624bb391db8c86f85b3d794913b
SamsonWang <zl.wang@foxmail.com>
2017-09-23 12:46:23 +0800

    tweak recentf

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,33 +1,26 @@
 ;; -*- coding:utf-8 -*-
-;; Misc Settings
+;; recentf setting
 
-
-;;================================================================
-;; recentf-mode settings
-;;================================================================
 ;; Keep a list of recently opened files
 (recentf-mode t)
 ;; recentf-mode
+(setq recentf-save-file (expand-file-name ".recentf" config-home))
+(setq recentf-auto-cleanup 'never)
 (setq recentf-keep '(file-remote-p file-readable-p))
-(setq recentf-max-saved-items 10
-      recentf-exclude '("/tmp/"
-                        "/ssh:"
-                        "/sudo:"
+(setq recentf-max-saved-items 10)
+
+;; strip $HOME from the front of recentf files
+(add-to-list 'recentf-filename-handlers 'abbreviate-file-name)
+
+(setq recentf-exclude '("/tmp/" "/ssh:" "/sudo:"
                         ;; ctags
                         "/TAGS$"
                         ;; global
-                        "/GTAGS$"
-                        "/GRAGS$"
-                        "/GPATH$"
+                        "/GTAGS$" "/GRAGS$" "/GPATH$"
                         ;; binary
-                        "\\.mkv$"
-                        "\\.mp[34]$"
-                        "\\.avi$"
-                        "\\.pdf$"
+                        "\\.mkv$" "\\.mp[34]$" "\\.avi$" "\\.pdf$"
                         ;; sub-titles
-                        "\\.sub$"
-                        "\\.srt$"
-                        "\\.ass$"
+                        "\\.sub$" "\\.srt$" "\\.ass$"
                         "/ido.last$"
                         ;; ~/.emacs.d/**/*.el included
                         ;; "/home/[a-z]\+/\\.[a-df-z]"
