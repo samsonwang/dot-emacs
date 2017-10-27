5d639cf07d1677c85e7efe0d01fce60ab53f8468
SamsonWang <zl.wang@foxmail.com>
2017-10-27 20:05:00 +0800

    rename function

--- lisp/sw-split-window.el
+++ lisp/sw-split-window.el
@@ -1,7 +1,7 @@
 
 ;; split window with certain ratio
 
-(defvar my-ratio-dict
+(defvar sw/ratio-dict
   '((1 . 1.61803398875)
     (2 . 2)
     (3 . 3)
@@ -9,14 +9,14 @@
     (5 . 0.61803398875))
   "retio dictionary")
 
-(defun sw-split-window-horizontally (&optional ratio)
+(defun sw/split-window-horizontally (&optional ratio)
   "Split window horizontally and resize the new window.
 Always focus bigger window."
   (interactive "P")
   (let* (ratio-val)
     (cond
      (ratio
-      (setq ratio-val (cdr (assoc ratio my-ratio-dict)))
+      (setq ratio-val (cdr (assoc ratio sw/ratio-dict)))
       (split-window-horizontally (floor (/ (window-body-width)
                                            (1+ ratio-val)))))
      (t
@@ -26,14 +26,14 @@ Always focus bigger window."
             (>= ratio-val 1))
         (windmove-right))))
 
-(defun sw-split-window-vertically (&optional ratio)
+(defun sw/split-window-vertically (&optional ratio)
   "Split window vertically and resize the new window.
 Always focus bigger window."
   (interactive "P")
   (let* (ratio-val)
     (cond
      (ratio
-      (setq ratio-val (cdr (assoc ratio my-ratio-dict)))
+      (setq ratio-val (cdr (assoc ratio sw/ratio-dict)))
       (split-window-vertically (floor (/ (window-body-height)
                                          (1+ ratio-val)))))
      (t
@@ -45,8 +45,8 @@ Always focus bigger window."
             (>= ratio-val 1))
         (windmove-down))))
 
-(global-set-key (kbd "C-x 2") 'sw-split-window-vertically)
-(global-set-key (kbd "C-x 3") 'sw-split-window-horizontally)
+(global-set-key (kbd "C-x 2") 'sw/split-window-vertically)
+(global-set-key (kbd "C-x 3") 'sw/split-window-horizontally)
 
 ;; sw-split-window ends here
 (provide 'sw-split-window)
