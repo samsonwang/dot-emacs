a2f21c6bffcdd92ab9f6b360ab64a819e4fbdd09
SamsonWang <zl.wang@foxmail.com>
2017-12-25 16:37:25 +0800

    put server auth file in temp directory

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,12 @@
 
 
 (use-package magit
+  :init
+  ;; magit requires server.el
+  ;; put server auth file in cache directory
+  (custom-set-variables
+   '(server-auth-dir (concat user-emacs-cache "server/")))
+  
   :bind
   (("C-c g s" . magit-status)
    ("C-c g g" . magit-dispatch-popup))
