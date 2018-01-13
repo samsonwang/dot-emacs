6b792bff2224ffbbaf225b3e30bc0c33df9cc323
Samson <zl.wang@qq.com>
2018-01-14 01:02:47 +0800

    use tab as 2 spaces in mmm-mode.

--- lisp/init-nikola.el
+++ lisp/init-nikola.el
@@ -1,22 +1,21 @@
 
 
 ;; nikola use mako as its template engine
-(use-package mmm-mako
-  :init
-  (require 'mmm-auto)
-  
-  (setq mmm-global-mode 'maybe)
-  (add-to-list 'auto-mode-alist '("\\.tmpl\\'" . html-mode))
-  (mmm-add-mode-ext-class 'html-mode "\\.tmpl\\'" 'mako)
+;; mmm-mode doesnot fit with use-package, don't know why
+(require 'mmm-mode)
+(require 'mmm-mako)
 
-  ;; use tab as tab in mmm-mode
-  :bind
-  (:map
-   mmm-mode-map
-   ("<tab>" . self-insert-command)
-   )
-  
-)
+(setq mmm-global-mode 'maybe)
+
+(add-to-list 'auto-mode-alist '("\\.tmpl\\'" . html-mode))
+(mmm-add-mode-ext-class 'html-mode "\\.tmpl\\'" 'mako)
+
+;; use tab as tab in mmm-mode
+(define-key mmm-mode-map
+  (kbd "<tab>") '(lambda () (interactive) (insert-char 32 2)))
+
+;(define-key mmm-mode-map
+;  (kbd "<tab>") '(lambda () (interactive) (insert-char 9 1)))
 
 (provide 'init-nikola)
 ;; init-nikola ends here
