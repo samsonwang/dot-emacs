223e0e1ff458932e9c27f110e258e26b5a555f32
SamsonWang <zl.wang@foxmail.com>
2018-01-18 13:12:28 +0800

    fix dired key binding in windows

---
+++ lisp/init-dired.el
@@ -0,0 +1,14 @@
+
+
+(use-package dired
+  :bind
+  (:map
+   dired-mode-map
+   ;; Q is bind to "dired-do-find-regex-and-replace"
+   ;; windows do not have grep by default, 
+   ("Q" . dired-do-query-replace-regexp)
+   )
+  )
+
+(provide 'init-dired)
+;; init-dired ends here
