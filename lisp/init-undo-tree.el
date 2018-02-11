4d7a41a6c367fc7c4643b9a3c0c19e264c01942b
SamsonWang <zl.wang@foxmail.com>
2018-02-11 15:16:49 +0800

    global undo tree mode

--- lisp/init-undo-tree.el
+++ lisp/init-undo-tree.el
@@ -3,10 +3,11 @@
 
 
 (use-package undo-tree
+  :diminish undo-tree-mode
   :bind
-  (("C-c u" . undo-tree-mode))
+  (("C-c u" . undo-tree-mode)))
 
-  )
+(global-undo-tree-mode)
 
 (provide 'init-undo-tree)
 
