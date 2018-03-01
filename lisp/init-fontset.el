f6e0254cb066e3c38b82281b94179007b0045486
samson <zl.wang@qq.com>
2018-03-01 23:06:34 +0800

    set fontset for linux GUI

--- lisp/init-fontset.el
+++ lisp/init-fontset.el
@@ -9,10 +9,13 @@
                       (font-spec :family chinese-name :size chinese-size))))
 
 (when *windows*
-   (set-fontset "Consolas" "微软雅黑" 16 16))
+  (set-fontset "Consolas" "微软雅黑" 16 16))
 
 (when *macintosh*
-   (set-fontset "Menlo" "Menlo" 16 16))
+  (set-fontset "Menlo" "Menlo" 16 16))
+
+(when *linux*
+  (set-fontset "Mono" "Sans" 16 16))
 
 (provide 'init-fontset)
 ;;; init-fontset.el
