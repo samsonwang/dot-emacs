39ea953b8716611c2bd0e6f35f27b50aec8e583a
SamsonWang <zl.wang@foxmail.com>
2018-03-19 21:19:21 -0400

    fix fontset in linux cli

--- lisp/init-fontset.el
+++ lisp/init-fontset.el
@@ -7,15 +7,13 @@
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font) charset
                       (font-spec :family chinese-name :size chinese-size))))
-
-(when *windows*
-  (set-fontset "Consolas" "微软雅黑" 16 16))
-
-(when *macintosh*
-  (set-fontset "Menlo" "Menlo" 16 16))
-
-(when *linux*
-  (set-fontset "Mono" "Sans" 16 16))
+(when (display-graphic-p) 
+  (when *windows*
+    (set-fontset "Consolas" "微软雅黑" 16 16))
+  (when *macintosh*
+    (set-fontset "Menlo" "Menlo" 16 16))
+  (when (and *linux* (display-graphic-p))
+    (set-fontset "Mono" "Sans" 16 16)))
 
 (provide 'init-fontset)
 ;;; init-fontset.el
