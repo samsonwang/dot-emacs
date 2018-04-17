ab8eb5c3eb6a951655820ee371b3a58c2a8dba28
SamsonWang <zl.wang@foxmail.com>
2018-04-17 20:05:31 +0800

    python indent offset

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -5,6 +5,7 @@
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
   :init
+  (setq-default python-indent-offset 4)
   (add-hook 'python-mode-hook
             (lambda ()
               (setq indent-tabs-mode t)
