d82f574b55aca2d7190df362be9e0106a8550de5
SamsonWang <zl.wang@foxmail.com>
2018-04-19 11:19:24 +0800

    better python mode indent

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,16 +1,19 @@
 
 
-
 (use-package python
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
   :init
-  (setq-default python-indent-offset 4)
-  (add-hook 'python-mode-hook
-            (lambda ()
-              (setq indent-tabs-mode t)
-              (setq tab-width 4)))
 
+  (defun python-mode-hook-func()
+    "hook func for python mode"
+    (setq python-indent-offset 4)
+;;  (setq indent-tabs-mode t)
+;;  (setq tab-width 4)
+    (linum-mode))
+
+  (add-hook 'python-mode-hook
+            #'python-mode-hook-func)
   )
 
 (provide 'init-python-mode)
