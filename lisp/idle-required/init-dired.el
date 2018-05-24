5361db72dc1a9261f9d9fd76f7ec54f1bb54bbca
SamsonWang <zl.wang@foxmail.com>
2018-05-24 17:01:41 +0800

    sort in dired mode

--- lisp/idle-required/init-dired.el
+++ lisp/idle-required/init-dired.el
@@ -1,13 +1,26 @@
 
-
 (use-package dired
   :bind
   (:map
    dired-mode-map
    ;; Q is bind to "dired-do-find-regex-and-replace"
-   ;; windows do not have grep by default, 
+   ;; windows do not have grep by default,
    ("Q" . dired-do-query-replace-regexp)
    )
+  :config
+  ;; helm-comp-read == completing-read
+  (defun dired-sort (criteria)
+    "sort-dired by different criteria by Robert Gloeckner"
+    (interactive
+     (list
+      (or (helm-comp-read "sort-by: "
+                          '("size(S)" "extension(X)" "creation-time(ct)"
+                            "access-time(ut)" "time(t)" "name()"))
+          "")))
+    (string-match ".*(\\(.*\\))" criteria)
+    (dired-sort-other
+     (concat dired-listing-switches
+             (match-string 1 criteria))))
   )
 
 (provide 'init-dired)
