9419a1a6340e03db8554e4f450b976330fa90cc3
SamsonWang <zl.wang@foxmail.com>
2018-07-31 14:54:52 +0800

    yas already contains this directory by default

--- lisp/idle-required/init-yasnippet.el
+++ lisp/idle-required/init-yasnippet.el
@@ -1,12 +1,10 @@
+
 (use-package yasnippet
   :diminish yas-minor-mode
-  :init
-  (setq yas-snippet-dirs
-        (expand-file-name "snippets" user-emacs-directory))
-
   :config
   (setq yas-verbosity 0)
   (yas-global-mode 1)
   (setq yas-verbosity 3))
 
 (provide 'init-yasnippet)
+
