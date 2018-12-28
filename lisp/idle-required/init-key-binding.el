0ad6dfcd645faf76a590327876945a2789714976
SamsonWang <wangzhilv@gmail.com>
2018-12-28 17:57:29 +0800

    update key bindings

--- lisp/idle-required/init-key-binding.el
+++ lisp/idle-required/init-key-binding.el
@@ -1,24 +1,21 @@
-
 ;; -*- coding:utf-8 -*-
 ;; Keybindings
 
-
-;; use ibuffer instead of list-buffers
-;;(global-set-key (kbd "C-x C-b") 'ibuffer)
-
 ;; eval buffer
-(global-set-key [f10] #'eval-buffer)
-
-;; Set C-x C-g to goto line
-;; (global-set-key (kbd "C-x g") #'goto-line)
+(global-set-key [f5] #'eval-buffer)
 
 ;; unset C-x m for mail, because it is easily mixed with C-x C-m
-(global-unset-key (kbd "C-x m"))
+;;(global-unset-key (kbd "C-x m"))
+
+;; set-mark-command is usually mixed with input
+(global-set-key (kbd "C-c m") #'set-mark-command)
+(global-set-key (kbd "C-c M-m") #'set-mark-command)
+
 
 ;; Unset C-z for suspend-frame
 (global-unset-key (kbd "C-z"))
 ;;(global-set-key (kbd "C-z") 'suspend-frame-ex)
-(global-set-key (kbd "C-x C-z") #'suspend-frame-ex)
+(global-set-key (kbd "C-c C-z") #'suspend-frame-ex)
 (defun suspend-frame-ex ()
   (interactive)
   (if (display-graphic-p)
@@ -26,7 +23,8 @@
 	(suspend-frame)))
 
 
-(global-set-key (kbd "C-c C-r") #'revert-buffer-no-confirm)
+(global-set-key (kbd "C-c r") #'revert-buffer-no-confirm)
+(global-set-key (kbd "C-c M-r") #'revert-buffer-no-confirm)
 ;; Source: http://www.emacswiki.org/emacs-en/download/misc-cmds.el
 (defun revert-buffer-no-confirm ()
   "Revert buffer without confirmation."
