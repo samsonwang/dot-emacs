0218c06cdc6de575856cc9b085fabc3acea2cc5c
SamsonWang <wangzhilv@gmail.com>
2019-01-29 10:59:58 +0800

    org mode verbatim

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -31,7 +31,7 @@
   (setq org-src-fontify-natively t)
 
   ;; @https://emacs.stackexchange.com/questions/13820/inline-verbatim-and-code-with-quotes-in-org-mode
-  (setcar (nthcdr 2 org-emphasis-regexp-components) " \t\r\n,")
+  (setcar (nthcdr 2 org-emphasis-regexp-components) " \t\r\n\"'")
   (org-set-emph-re 'org-emphasis-regexp-components
                    org-emphasis-regexp-components)
 
