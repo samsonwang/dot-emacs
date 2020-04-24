f9e2914b4585d9ade70ecffe98e3ec212f9802cd
Samson Wang <wangzhilv@gmail.com>
2020-04-24 21:35:06 +0800

    show message when toggle window margin

---
+++ lisp/idle-required/init-playground.el
@@ -0,0 +1,29 @@
+(defun xah-show-in-desktop ()
+  "Show current file in desktop.
+ (Mac Finder, Windows Explorer, Linux file manager)
+ This command can be called when in a file or in `dired'.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2020-02-13"
+  (interactive)
+  (let (($path (if (buffer-file-name) (buffer-file-name)
+                 (shell-quote-argument default-directory))))
+    (cond
+     ((string-equal system-type "windows-nt")
+      (w32-shell-execute "open" default-directory))
+     ((string-equal system-type "darwin")
+      (if (eq major-mode 'dired-mode)
+          (let (($files (dired-get-marked-files )))
+            (if (eq (length $files) 0)
+                (shell-command (concat "open " (shell-quote-argument default-directory)))
+              (shell-command (concat "open -R " (shell-quote-argument (car (dired-get-marked-files )))))))
+        (shell-command
+         (concat "open -R " $path))))
+     ((string-equal system-type "gnu/linux")
+      (let ((process-connection-type nil)
+            (openFileProgram (if (file-exists-p "/usr/bin/gvfs-open")
+                                 "/usr/bin/gvfs-open"
+                               "/usr/bin/xdg-open")))
+        (start-process "" nil openFileProgram $path))
+      ;; (shell-command "xdg-open .") ;; 2013-02-10 this sometimes froze emacs till the folder is closed. eg with nautilus
+      ))))
--- lisp/idle-required/init-window-margin.el
+++ lisp/idle-required/init-window-margin.el
@@ -7,14 +7,22 @@
   (if (null (cdr (window-margins)))
       (cond
        (margin-width
-        (set-window-margins nil 0 (- (window-body-width) margin-width)))
+        (set-window-margins nil 0 (- (window-body-width) margin-width))
+        (message (format "window margins set to %d" margin-width)))
        (t
-        (set-window-margins nil 0 (- (window-body-width) fill-column))))
-    (set-window-margins nil 0 0)))
+        (set-window-margins nil 0 (- (window-body-width) fill-column))
+        (message (format "window margins set to %d" fill-column))))
+    (progn
+      (set-window-margins nil 0 0)
+      (message "window margins cancelled"))))
 
 (global-set-key (kbd "C-x 4") #'toggle-window-margin-right)
 
 (setq-default fill-column 90)
 
-(provide 'init-window-margin)
+;; TODO:
+;; 1. update window margins when window change
+;; 2. cancel window margins only when margin equals target
+;; 3. consider use 90 instead of 'fill-column'
 
+(provide 'init-window-margin)
