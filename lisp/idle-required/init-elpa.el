192038c2e8c4216c719dd1c7b9bcc83337e33d7e
Samson Wang <wangzhilv@gmail.com>
2020-08-31 20:43:11 +0800

    disable package signature

--- lisp/idle-required/init-elpa.el
+++ lisp/idle-required/init-elpa.el
@@ -60,8 +60,8 @@
 ;;              '("melpa-stable" .
 ;;                "http://mirrors.163.com/elpa/melpa-stable/"))
 
-(unless *emacs26*
-  (setq package-check-signature nil))
+;; (unless *emacs26* )
+(setq package-check-signature nil)
 
 (require 'cl-lib)
 (defun package-install-ex (my-packages)
