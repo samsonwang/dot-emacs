92ad9cbd5dc3a1594b1e24b6f5400cb5514dc532
samsonwang <wangzhilv@gmail.com>
2026-01-20 10:16:46 +0800

    fix garbage collect error

--- early-init.el
+++ early-init.el
@@ -4,9 +4,11 @@
 
 ;; adjust garbage collection thresholds during startup, and thereafter
 (defconst normal-file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil
-      ;; gc-cons-percentage 0.5
-      gc-cons-threshold most-positive-fixnum) ;; 1GB
+(setq file-name-handler-alist nil)
+
+(setq gc-cons-percentage 0.6)
+(setq gc-cons-threshold (* 1024 1024 1024)) ;; 1GB
+
 (defun emacs-startup-hook-func ()
   (setq file-name-handler-alist normal-file-name-handler-alist))
 (add-hook 'emacs-startup-hook #'emacs-startup-hook-func)
--- lisp-idle/init-gc.el
+++ lisp-idle/init-gc.el
@@ -1,26 +1,22 @@
 ;; -*- coding: utf-8 -*-
 
 ;; show gc message
-;; (setq garbage-collection-messages t)
+(setq garbage-collection-messages t)
 
 ;; Set garbage collection threshold to 1GB.
 ;; (setq gc-cons-threshold #x40000000)
 ;; (setq gc-cons-percentage 0.5)
 
-(defmacro eval-time (&rest body)
-  "Measure and return the time it takes evaluating BODY."
-  `(let ((time (current-time)))
-     ,@body
-     (float-time (time-since time))))
+(defun gc-with-timing ()
+  "Execute garbage collection and show elapsed time (only when garbage-collection-messages is non-nil)"
+  (interactive)
+  (let ((start-time (float-time)))
+    (garbage-collect)
+    (let ((elapsed-time (- (float-time) start-time)))
+      (when garbage-collection-messages
+        (message "Garbage collection completed, elapsed: %.3f seconds" elapsed-time)))))
 
-;; when idle for seconds run gc
 (defvar gc-timer
-  (run-with-idle-timer 60 t #'garbage-collect))
-
-;; (defvar gc-timer
-;;   (run-with-idle-timer 15 t
-;;                        (lambda ()
-;;                          (message "Garbage Collector has run for %.06f sec"
-;;                                   (eval-time (garbage-collect))))))
+  (run-with-idle-timer 300 t #'gc-with-timing)) ;; gc every 5 min
 
 (provide 'init-gc)
